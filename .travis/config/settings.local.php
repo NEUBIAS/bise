9c9b3ee98b0a80927d4a3668c5cf0f0f1eff06aa
Kota Miura <miura@cmci.info>
2018-03-28 00:09:46 +0200

    added travis CI https://github.com/Lullabot/drupal8ci#travis-ci

---
+++ .travis/config/settings.local.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'mariadb',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
